bfe2d63f24a8fddcc1cf6bcca0e0e35a526d3c2f
Ormazd <pbisset@protonmail.com>
2023-05-24 17:13:27 +0200

    feat: Added ban functions

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -2,15 +2,23 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+//Access Control imports
+import "@openzeppelin/contracts/access/IAccessControl.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 //test
 
-contract ArbiPool is ERC20 {
+abstract contract ArbiPool is AccessControl, ERC20 {
     uint public unlockTime;
     address payable public owner;
     IERC20 public usdt;
 
-    event buy(uint amount);
+    bytes32 public constant BANNED_ROLE = keccak256("BANNED");
+    bytes32 public constant CUSTOM_ADMIN = keccak256("ADMIN");
 
     constructor(address _usdt, uint _unlockTime) ERC20("ArbiPool", "ARP") {
         require(
@@ -20,10 +28,13 @@ contract ArbiPool is ERC20 {
         usdt = IERC20(_usdt);
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
+        _setRoleAdmin(CUSTOM_ADMIN, DEFAULT_ADMIN_ROLE);
+        _grantRole(CUSTOM_ADMIN, msg.sender);
         _mint(msg.sender, 100000 * (10 ** uint256(decimals())));
     }
 
     function buy(uint256 _amount) external {
+        require(!isBanned(msg.sender), "Can't buy if your address is banned");
         require(usdt.transferFrom(msg.sender, address(this), _amount), "transfer failed");
         _mint(msg.sender, _amount);
     }
@@ -33,6 +44,19 @@ contract ArbiPool is ERC20 {
         require(block.timestamp >= unlockTime, "Contract is still locked");
         
         usdt.transfer(owner, _amount);
+    }
+
+    function banAddress(address accountToBan) external {
+        require(isAdmin(msg.sender), "Restricted to admins");
+        grantRole(BANNED_ROLE, accountToBan);
+    }
+
+    function isAdmin(address account) public virtual view returns (bool) {
+        return hasRole(CUSTOM_ADMIN, account);
+    }
+
+    function isBanned(address account) public virtual view returns (bool) {
+        return hasRole(BANNED_ROLE, account);
     }
 }
 
